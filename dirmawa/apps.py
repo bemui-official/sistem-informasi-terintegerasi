390020e9dc69e4abeb39691482ed124a524a2729
mikasrahwono <mikasuryof@outlook.com>
2022-03-24 13:38:36 +0700

    Initial structure setup

---
+++ dirmawa/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DirmawaConfig(AppConfig):
+    name = 'dirmawa'
---
+++ penyetoran/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PenyetoranConfig(AppConfig):
+    name = 'penyetoran'
